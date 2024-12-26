147219bd291a3cc5b7acba8b7dd8557276d9ba4e
selım <asimenesturkmen2021@gmail.com>
2024-12-26 01:21:35 +0100

    elhamdulillah

--- webapp/component.js
+++ webapp/component.js
@@ -1,41 +1,44 @@
+
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
- ], (UIComponent, JSONModel, ResourceModel) => {
-    "use strict";
- 
-    return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
-       metadata : {
-          "rootView": {
-             "viewName": "sap.ui.demo.walkthrough.view.App",
-             "type": "XML",
-             "async":true,
-             "id": "app"
-          }
-       },
- 
-       init() {
-          // call the init function of the parent
-          UIComponent.prototype.init.apply(this, arguments);
-          // set data model
-          const oData = {
-             recipient : {
-                name : "UI5"
-             }
-          };
-          const oModel = new JSONModel(oData);
-          this.setModel(oModel);
- 
-          // set i18n model
-          const i18nModel = new ResourceModel({
-             bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
-             supportedLocales:[""],
-             fallbackLocales:""
+    "sap/ui/model/resource/ResourceModel",
+    "./controller/HelloDialog"
 
-          });
-          this.setModel(i18nModel, "i18n");
-       }
-    });
- });
- 
+], function (UIComponent, JSONModel, ResourceModel,HelloDialog) {
+    
+    "use strict";
+    
+    return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+        metadata : {
+            manifest: "json"
+        },
+        init : function () {
+            // call the init function of the parent
+            UIComponent.prototype.init.apply(this, arguments);
+            // set data models
+            var oData = {
+                recipient: {
+                    name: "UI5"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.setModel(oModel);
+             
+            
+            // set dialog
+             this._helloDialog = new HelloDialog(this.getRootControl());
+            },
+
+            exit: function () {
+                this._helloDialog.destroy();
+                delete this._helloDialog;
+            },
+            
+            openHelloDialog : function () {
+                this._helloDialog.open();
+            } 
+            
+    
+        });
+});
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,24 +1,14 @@
-sap.ui.define(
-  [
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel",
-  ],
-  (Controller, MessageToast, JSONModel, ResourceModel) => {
-    "use strict";
+sap.ui.define([
+  "sap/ui/core/mvc/Controller",
 
-    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-      onShowHello() {
-        // read msg from i18n model
-        const oBundle = this.getView().getModel("i18n").getResourceBundle();
-        const sRecipient = this.getView()
-          .getModel().getProperty("/recipient/name");
-        const sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-        // show message
-        MessageToast.show(sMsg);
-      },
-    });
+], function (Controller) {
+  "use strict"
+  
+  return Controller.extend("sap.ui.demo.walkthrough.App", {
+   
+    onOpenDialog: function () {
+      this.getOwnerComponent().openHelloDialog();
   }
-);
+
+  });
+});
