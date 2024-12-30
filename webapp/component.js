b8d117f6ff697a1926b09f86caf3433716b293cc
selım <asimenesturkmen2021@gmail.com>
2024-12-31 00:18:36 +0100

    Device_Adaptation

--- webapp/component.js
+++ webapp/component.js
@@ -3,10 +3,9 @@ sap.ui.define([
     "sap/ui/core/UIComponent",
     "sap/ui/model/json/JSONModel",
     "sap/ui/model/resource/ResourceModel",
-    "./controller/HelloDialog"
-
-], function (UIComponent, JSONModel, ResourceModel,HelloDialog) {
-    
+    "./controller/HelloDialog",
+    "sap/ui/Device"
+], function (UIComponent, JSONModel, ResourceModel, HelloDialog, Device) {
     "use strict";
     
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
@@ -16,6 +15,7 @@ sap.ui.define([
         init : function () {
             // call the init function of the parent
             UIComponent.prototype.init.apply(this, arguments);
+            
             // set data models
             var oData = {
                 recipient: {
@@ -25,6 +25,11 @@ sap.ui.define([
             var oModel = new JSONModel(oData);
             this.setModel(oModel);
              
+             // set device model
+             var oDeviceModel = new JSONModel(Device);
+             oDeviceModel.setDefaultBindingMode("OneWay");
+             this.setModel(oDeviceModel, "device");
+ 
             
             // set dialog
              this._helloDialog = new HelloDialog(this.getRootControl());
